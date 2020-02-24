cd7284a0030774dc15e5b712b6eb1eff5ccfa0c9
Josephine <test@TESTs-MacBook-Pro.local>
2020-02-24 19:15:19 +0200

    Adding comments

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
-var randomNumber1 = Math.floor(Math.random() *6) +1;
-var randomNumber = Math.floor(Math.random() *6) +1;
-var player1Image = "images/dice"+randomNumber1+".png";
-var player2Image ="images/dice"+randomNumber+".png";
+//storing random value in different variables for the two players
+var randomNumber1 = Math.floor(Math.random() *6) +1;// 1-6
+var randomNumber2 = Math.floor(Math.random() *6) +1;
 
+//using concatenation to randomize the dice selection
+var player1Image = "images/dice"+randomNumber1+".png";//images/dice1.png - images/dice6.png
+var player2Image ="images/dice"+randomNumber2+".png";
+
+//selecting image through the class in html and giving it a new attribute
 document.querySelector('.image1').setAttribute('src', player1Image);
 document.querySelector('.image2').setAttribute('src',player2Image);
-
+//changing H1 when either of the player wins or when both players have same values.
 if(player1Image > player2Image){
     document.querySelector('#title').innerHTML = "🚩Player One wins!🥳";
 }else if(player2Image > player1Image){
